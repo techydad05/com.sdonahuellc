7fc98c5ce00e9bf4969112caa63e5fca8d331351
Ivan <techydad05@gmail.com>
2024-04-12 15:22:16 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,20 @@
 			behavior: 'smooth'
 		});
 	};
+
+	let scrolled;
+	function getScrollPercent() {
+		let h = document.documentElement,
+			b = document.body,
+			st = 'scrollTop',
+			sh = 'scrollHeight';
+		scrolled = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
+		// console.log(scrolled);
+		return scrolled;
+	}
 </script>
+
+<svelte:window on:scroll={() => getScrollPercent()} />
 
 <div class="navbar bg-base-100 h-[20vh]">
 	<div class="navbar-start"></div>
@@ -19,7 +32,7 @@
 	<div class="navbar-end"></div>
 </div>
 <div class="flex flex-col justify-start items-center h-[80vh]">
-	<div class="h-[60vh] w-full">
+	<div class="h-[80%] w-full">
 		<Slidy
 			slides={[
 				{ thumbnail: '/yard1.jpg' },
@@ -33,22 +46,22 @@
 			background
 			snap={'center'}
 			--slidy-slide-width={'100%'}
-			--slidy-slide-gap={'4px'}
+			--slidy-slide-gap={'0'}
 			--slidy-slide-height={'100%'}
 			--slidy-slide-radius={'none'}
 		/>
 	</div>
-	<!-- <button class="btn btn-outline btn-primary mt-[3%]">Learn More</button> -->
-	<button
-		data-scroll="#section-2"
-		class="btn btn-outline btn-primary mt-[3%]"
-		on:click={scrollIntoView}
-	>
-		Learn More</button
-	>
+	<div class="h-[20%] flex items-center justify-center">
+        <button data-scroll="#section-2" class="btn btn-outline btn-primary" on:click={scrollIntoView}>
+            Learn More</button
+        >
+    </div>
 </div>
 <div class="flex h-[50vh] mb-[20%]">
-	<div class="hero h-full bg-no-repeat" style="background-image: url(/yardbg.jpg);">
+	<div
+		class="hero h-full bg-no-repeat"
+		style={`background-image: url(/yardbg.jpg);background-size: auto ${160 + scrolled}%;`}
+	>
 		<div class="hero-overlay bg-opacity-60"></div>
 		<div class="hero-content text-center text-neutral-content">
 			<div class="max-w-md">
@@ -58,15 +71,38 @@
 	</div>
 </div>
 <div id="section-2" class="flex h-[100svh]">
-	<div
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content flex-col lg:flex-row">
+			<img
+				src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+				class="hidden md:block max-w-sm rounded-lg shadow-2xl"
+			/>
+			<div class="bg-[url('https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg')] bg-no-repeat bg-cover bg-center text-white p-10 md:bg-none md:p-0 md:text-black">
+				<h1 class="text-5xl font-bold">A Little Bit About Me</h1>
+				<p class="py-6">
+					The moment I became Spider-Man is etched into my memory like a vivid dream. It all began
+					with a simple visit to Oscorp Industries, where a chance encounter with a genetically
+					modified spider altered the course of my life forever. As its venom coursed through my
+					veins, I felt a surge of newfound strength, heightened senses, and an uncanny agility.
+					Confusion soon gave way to realization as I found myself scaling walls and swinging
+					effortlessly through the city streets. Embracing my newfound identity, I vowed to use my
+					powers for good, navigating the dual identities of Peter Parker and the friendly
+					neighborhood Spider-Man with a sense of purpose and responsibility, determined to make a
+					positive impact on the world around me..
+				</p>
+				<button class="btn btn-primary">Get Started</button>
+			</div>
+		</div>
+	</div>
+	<!-- <div
 		class="hero min-h-screen"
 		style="background-image: url('');"
 	>
 		<div class="hero-overlay bg-opacity-60"></div>
 		<div class="hero-content text-center text-neutral-content">
 			<div class="max-w-md">
-				<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-				<p class="mb-5">
+				<h1 class="mb-5 text-5xl font-bold">About Me</h1>
+				<p class="mb-5 text-left">
 					The moment I became Spider-Man is etched into my memory like a vivid dream. It all began
 					with a simple visit to Oscorp Industries, where a chance encounter with a genetically
 					modified spider altered the course of my life forever. As its venom coursed through my
@@ -80,12 +116,12 @@
 				<button class="btn btn-outline btn-primary">Contact Me</button>
 			</div>
 		</div>
-	</div>
+	</div> -->
 </div>
 
 <style>
 	:global(.slidy-slide.bg) {
-		background-position: bottom !important;
+		background-position: top !important;
 	}
 	:global(.slidy-slides) {
 		padding: 0 5px;
