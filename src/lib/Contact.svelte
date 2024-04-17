9cea3defd3af0e60711daf6b68353c27a37ff0b8
Ivan <techydad05@gmail.com>
2024-04-17 13:51:03 -0400

    adding contact form

---
+++ src/lib/Contact.svelte
@@ -0,0 +1,59 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+        
+        const btn = document.getElementById('button');
+        
+		document.getElementById('form').addEventListener('submit', function (event) {
+            emailjs.init('user_sfHHqVix3VlKerTBKwc66');
+			event.preventDefault();
+
+			btn.value = 'Sending...';
+
+			const serviceID = 'sdonahue';
+			const templateID = 'template_8tfe3th';
+
+			emailjs.sendForm(serviceID, templateID, this).then(
+				() => {
+					btn.value = 'Send Email';
+					alert('Sent!');
+				},
+				(err) => {
+					btn.value = 'Send Email';
+					alert(JSON.stringify(err));
+				}
+			);
+		});
+	});
+</script>
+
+<head>
+	<script
+		type="text/javascript"
+		src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"
+	></script>
+</head>
+
+<div class="contact">
+	<form id="form">
+		<div class="field">
+			<label for="from_name">from_name</label>
+			<input type="text" name="from_name" id="from_name" />
+		</div>
+		<div class="field">
+			<label for="from_phone">from_phone</label>
+			<input type="text" name="from_phone" id="from_phone" />
+		</div>
+		<div class="field">
+			<label for="from_email">from_email</label>
+			<input type="text" name="from_email" id="from_email" />
+		</div>
+		<div class="field">
+			<label for="message">message</label>
+			<input type="text" name="message" id="message" />
+		</div>
+
+		<input type="submit" id="button" value="Send Email" />
+	</form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { Slidy } from '@slidy/svelte';
 	import '@slidy/svelte/dist/slidy.css';
-
+	import Contact from '../lib/Contact.svelte';
+	const Plugins = import('@slidy/plugins');
+	
 	const scrollIntoView = ({ target }) => {
 		const el = document.querySelector(target.dataset.scroll);
 		if (!el) return;
@@ -24,40 +26,47 @@
 
 <svelte:window on:scroll={() => getScrollPercent()} />
 
-<div class="navbar bg-base-100 h-[20vh]">
-	<div class="navbar-start"></div>
-	<div class="navbar-center h-full">
-		<a class="h-full"><img class="h-full" src="/sdonahue-logo.png" alt="" /></a>
+<div class="h-screen">
+	<div class="navbar bg-base-100 h-[20%]">
+		<div class="navbar-start"></div>
+		<div class="navbar-center h-full">
+			<a class="h-full"><img class="h-full" src="/sdonahue-logo.png" alt="" /></a>
+		</div>
+		<div class="navbar-end"></div>
 	</div>
-	<div class="navbar-end"></div>
-</div>
-<div class="flex flex-col justify-start items-center h-[80vh]">
-	<div class="h-[80%] w-full">
-		<Slidy
-			slides={[
-				{ thumbnail: '/yard1.jpg' },
-				{ thumbnail: '/yard2.jpg' },
-				{ thumbnail: '/yard3.jpg' }
-			]}
-			getImgSrc={(item) => item.thumbnail}
-			arrows
-			loop
-			counter
-			background
-			snap={'center'}
-			--slidy-slide-width={'100%'}
-			--slidy-slide-gap={'0'}
-			--slidy-slide-height={'100%'}
-			--slidy-slide-radius={'none'}
-		/>
+	<div class="flex flex-col justify-start items-center h-[70%]">
+		<div class="h-full w-full">
+			{#await Plugins}
+				LOading...
+			{:then Plugins}
+				<Slidy
+					slides={[
+						{ thumbnail: '/yard1.jpg' },
+						{ thumbnail: '/yard2.jpg' },
+						{ thumbnail: '/yard3.jpg' }
+					]}
+					getImgSrc={(item) => item.thumbnail}
+					arrows
+					loop
+					counter
+					background
+					plugins={[Plugins.autoplay({ autoplay: true, duration: 3000 })]}
+					snap={'center'}
+					--slidy-slide-width={'100%'}
+					--slidy-slide-gap={'0'}
+					--slidy-slide-height={'100%'}
+					--slidy-slide-radius={'none'}
+				/>
+			{/await}
+		</div>
 	</div>
-	<div class="h-[20%] flex items-center justify-center">
-        <button data-scroll="#section-2" class="btn btn-outline btn-primary" on:click={scrollIntoView}>
-            Learn More</button
-        >
-    </div>
+	<div class="h-[10%] flex items-center justify-center">
+		<button data-scroll="#section-2" class="btn btn-outline btn-primary" on:click={scrollIntoView}>
+			Learn More</button
+		>
+	</div>
 </div>
-<div class="flex h-[50vh] mb-[20%]">
+<div class="flex h-[70vh]">
 	<div
 		class="hero h-full bg-no-repeat"
 		style={`background-image: url(/yardbg.jpg);background-size: auto ${160 + scrolled}%;`}
@@ -70,6 +79,7 @@
 		</div>
 	</div>
 </div>
+<Contact />
 <div id="section-2" class="flex h-[100svh]">
 	<div class="hero min-h-screen bg-base-200">
 		<div class="hero-content flex-col lg:flex-row">
@@ -77,7 +87,9 @@
 				src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
 				class="hidden md:block max-w-sm rounded-lg shadow-2xl"
 			/>
-			<div class="bg-[url('https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg')] bg-no-repeat bg-cover bg-center text-white p-10 md:bg-none md:p-0 md:text-black">
+			<div
+				class="bg-[url('https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg')] bg-no-repeat bg-cover bg-center text-white p-10 md:bg-none md:p-0 md:text-black"
+			>
 				<h1 class="text-5xl font-bold">A Little Bit About Me</h1>
 				<p class="py-6">
 					The moment I became Spider-Man is etched into my memory like a vivid dream. It all began
@@ -94,29 +106,6 @@
 			</div>
 		</div>
 	</div>
-	<!-- <div
-		class="hero min-h-screen"
-		style="background-image: url('');"
-	>
-		<div class="hero-overlay bg-opacity-60"></div>
-		<div class="hero-content text-center text-neutral-content">
-			<div class="max-w-md">
-				<h1 class="mb-5 text-5xl font-bold">About Me</h1>
-				<p class="mb-5 text-left">
-					The moment I became Spider-Man is etched into my memory like a vivid dream. It all began
-					with a simple visit to Oscorp Industries, where a chance encounter with a genetically
-					modified spider altered the course of my life forever. As its venom coursed through my
-					veins, I felt a surge of newfound strength, heightened senses, and an uncanny agility.
-					Confusion soon gave way to realization as I found myself scaling walls and swinging
-					effortlessly through the city streets. Embracing my newfound identity, I vowed to use my
-					powers for good, navigating the dual identities of Peter Parker and the friendly
-					neighborhood Spider-Man with a sense of purpose and responsibility, determined to make a
-					positive impact on the world around me.
-				</p>
-				<button class="btn btn-outline btn-primary">Contact Me</button>
-			</div>
-		</div>
-	</div> -->
 </div>
 
 <style>
@@ -125,5 +114,12 @@
 	}
 	:global(.slidy-slides) {
 		padding: 0 5px;
+	}
+	:global(.slidy) {
+		gap: 0 !important;
+	}
+	:global(autoplay-button) {
+		/* z-index: 20 !important; */
+		display: none;
 	}
 </style>
